21d44a49e1460bdbaccd1f19d9598a09182a4d43
CanerGulduren <dngnhntr@gmail.com>
2023-01-29 16:55:06 +0300

    Make delete button
    Delete button gain function

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
-const txtArea = document.getElementById("text-box")
-const todoField = document.getElementById("todo-field")
-const form = document.getElementById("todo-form")
+const txtArea = document.getElementById("text-box");
+const todoField = document.getElementById("todo-field");
+const form = document.getElementById("todo-form");
 
+form.addEventListener("submit", createTodo);
 
-form.addEventListener("submit", createTodo)
+function createTodo(e) {
+  e.preventDefault();
 
-function createTodo(e){
-    e.preventDefault()
-   
-    /*Stop if no string enter*/
-    let txtAreaValue = txtArea.value
-    let todoText = txtAreaValue.trim()
-    if(todoText.length === 0){
-        return
-    }
+  /*Stop if no string enter*/
+  let txtAreaValue = txtArea.value;
+  let todoText = txtAreaValue.trim();
+  if (todoText.length === 0) {
+    return;
+  }
 
-    newTodo()
+  newTodo();
+  deleteTodo();
 }
 
-function newTodo(){
-    /*New delete button*/
-    let createNewDltBtn = new Image()
-    createNewDltBtn.src = "img/reject.png"
-    createNewDltBtn.classList.add("new-dlt-btn")
+function newTodo() {
+  /*New delete button*/
+  let createNewDltBtn = new Image();
+  createNewDltBtn.src = "img/reject.png";
+  createNewDltBtn.classList.add("new-dlt-btn");
 
-    /*New li*/
-    let createNewTodo = document.createElement("li")
-    let newTodoInfo = txtArea.value
-    createNewTodo.innerHTML = newTodoInfo
-    createNewTodo.classList.add("new-todo")
-    createNewTodo.append(createNewDltBtn)
+  /*New li*/
+  let createNewTodo = document.createElement("li");
+  let newTodoInfo = txtArea.value;
+  createNewTodo.innerHTML = newTodoInfo;
+  createNewTodo.classList.add("new-todo");
+  createNewTodo.append(createNewDltBtn);
 
-    todoField.append(createNewTodo)
-    txtArea.value = ""
+  todoField.append(createNewTodo);
+  txtArea.value = "";
+}
+
+function deleteTodo() {
+  let deleteBtn = Array.from(document.getElementsByClassName("new-dlt-btn"));
+  deleteBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+      button.parentElement.classList.add("hide");
+    });
+  });
 }
